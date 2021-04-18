6b05a054c22dbf97521393174e238cb8ed30716c
darshan <darshu8314@gmail.com>
2021-04-18 18:22:54 +0530

    employer admin pannels added

--- src/Components/Pages/RegisterAdd.jsx
+++ src/Components/Pages/RegisterAdd.jsx
@@ -8,7 +8,7 @@ function RegisterAdd() {
     return isAuth? null : (
         <div className="flexBox registerAdd">
         <div className="flexBox registerAddImg">
-          <img src="https://lh3.googleusercontent.com/proxy/PPcrCXyyB67jcsqPIkj8kFZb0nGK2koqjfyJdtEpSo29dQ-maKFoxCUWWAt9zYZJjrF8TmTZTKrjnkzc5bQpH4EwKNRLwCd9dWEEt3q_LhmRj3txDd_cWafJg8md" alt="user"/>
+          <img src="https://lh3.googleusercontent.com/proxy/iTu0RcJeQy4iiayqJcKwL2KyWsSVTKgTO7q0qAbo7oB8b8shxbQAUnpKkkgdAi0sdv_DfrP5M9MgMDms-DdP6TBAXU-dC-OH2F-mHNNTfwpMPXkr_OI_Kf4anMx4" alt="user"/>
           <p> Not Registered on monster yet?</p>
           </div>
        
